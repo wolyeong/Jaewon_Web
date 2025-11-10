993e7c523c9dda906f9ac5430b95f7d2b4ffb304
jaewon_pc <jaewonson26@gmail.com>
2025-11-11 02:55:50 +0900

    feat: 실제 사용할 이미지를 활용한 테스트

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ export const metadata: Metadata = {
       'DeepAiBit is an AI-driven digital healthcare company dedicated to designing healthier lifestyles for everyone.',
     images: [
       {
-        url: 'https://jaewonstore.vercel.app/image.png',
+        url: 'https://jaewonstore.vercel.app/gaitwise-logo.svg',
         alt: 'DeepAiBit 이미지',
       },
     ],
