a04541fdb5bad71932702f2c090f06cbb370fb53
jaewon_pc <jaewonson26@gmail.com>
2025-11-11 02:05:04 +0900

    feat: 메타데이터 테스트용 커밋

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,17 @@
 // app/layout.tsx (서버 컴포넌트)
+import type { Metadata } from 'next'
 import './globals.css'
 import ClientProviders from './ClientProviders'
+
+export const metadata: Metadata = {
+  title: '제목',
+  description: '설명',
+  openGraph: {
+    title: '온그래프 제목',
+    description: '온그래프 설명',
+    images: 'png/image.png',
+  },
+}
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
